3cac4d459001f953eca384e2028359f727a28ff3
andrelmreis <andreluiz2207@hotmail.com>
2015-06-04 16:22:49 -0300

    Second commit

--- plot2.R
+++ plot2.R
@@ -6,9 +6,6 @@ closeAllConnections()
 # Open PNG device
 png("plot2.png",width = 480, height = 480, units = "px")
 
-# Change margin parameters
-par(mar=c(4.1,4.1,2,2))
-
 # Make Line plot of Time vs Global Active Power
 plot(strptime(paste(power_consumption$Date,power_consumption$Time),format="%d/%m/%Y %H:%M:%S",tz="GMT"),power_consumption$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab="")
 
